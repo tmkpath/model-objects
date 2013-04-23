f2ab8f86bb4ff307c7422e7666f348399e6c6ef8
Sean Morris <sean.smorris@gmail.com>
2013-04-23 16:45:17 -0700

    FD updates and conformity to xsd

--- src/test/java/edu/berkeley/path/model_objects/scenario/FundamentalDiagramSetTest.java
+++ src/test/java/edu/berkeley/path/model_objects/scenario/FundamentalDiagramSetTest.java
@@ -1,7 +1,6 @@
 package edu.berkeley.path.model_objects.scenario;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +8,6 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
-import edu.berkeley.path.model_objects.scenario.FundamentalDiagramSet;
 
 public class FundamentalDiagramSetTest {
 	FundamentalDiagramSet set;
@@ -18,18 +16,18 @@ public class FundamentalDiagramSetTest {
 	public void setUp(){
 		set = new FundamentalDiagramSet();
 		set.setDescription("test set");
-		set.setProjectId(2);
+		set.setProjectId(2L);
 		set.setId(1);
 		set.setName("Test Set Name");
 		set.setModStamp("1970-01-01 00:00:00");
 		
 		List<FundamentalDiagramProfile> profiles = new ArrayList<FundamentalDiagramProfile>();
-		profiles.add(createFundamentalDiagramProfile(1,3600,300,1));
-		profiles.add(createFundamentalDiagramProfile(2,3660,300,2));
-		profiles.add(createFundamentalDiagramProfile(3,3720,300,3));
+		profiles.add(createFundamentalDiagramProfile(1,3600,300));
+		profiles.add(createFundamentalDiagramProfile(2,3660,300));
+		profiles.add(createFundamentalDiagramProfile(3,3720,300));
 		
 		set.setListOfFundamentalDiagramProfiles(profiles);
-		set.setFundamentalDiagramProfile(createFundamentalDiagramProfile(1,3600,300,1));
+		set.setFundamentalDiagramProfile(createFundamentalDiagramProfile(1,3600,300));
 		
 	}
 
@@ -43,7 +41,41 @@ public class FundamentalDiagramSetTest {
 		assertEquals(4, set.getListOfFundamentalDiagramProfiles().size());
 	}
 	
-	private Object createFundamentalDiagramProfile(int i, int j, int k, int l) {
-		// TODO Auto-generated method stub
-		return null;
+	private FundamentalDiagramProfile createFundamentalDiagramProfile(int linkId, double dt, double startTime) {
+		List<FundamentalDiagram> list = new ArrayList<FundamentalDiagram>();
+		list.add(createFundamentalDiagram(10,10,10,10,10,10,10,10,10));
+		list.add(createFundamentalDiagram(20,20,20,20,20,20,20,20,20));
+		list.add(createFundamentalDiagram(30,30,30,30,30,30,30,30,30));
+
+		
+		FundamentalDiagramProfile p = new FundamentalDiagramProfile();
+		p.setFundamentalDiagramList(list);
+		p.setLinkId(linkId);
+		p.setStartTime(startTime);
+		p.setDt(dt);
+		return p;
 	}
+	
+	private FundamentalDiagram createFundamentalDiagram(int cap, 
+			double speed, 
+			double crit,
+			double cong,
+			double jam,
+			double drop,
+			double stdCong,
+			double stdFree,
+			double stdCap
+			){
+		FundamentalDiagram d = new FundamentalDiagram();
+		d.setCapacity(cap);
+		d.setFreeFlowSpeed(speed);
+		d.setCriticalSpeed(crit);
+		d.setCongestionSpeed(cong);
+		d.setJamDensity(jam);
+		d.setCapacityDrop(drop);
+		d.setStdDevCongestionSpeed(stdCong);
+		d.setStdDevFreeFlowSpeed(stdFree);
+		d.setStdDevCapacity(stdCap);
+		return d;
+	}
+}
