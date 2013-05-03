d2d7a4aea1cb667f9d213d60bb4cd0f2387e90c7
Sean Morris <sean.smorris@gmail.com>
2013-05-03 13:43:57 -0700

    QA Sensors

--- src/main/java/edu/berkeley/path/model_objects/scenario/SensorSet.java
+++ src/main/java/edu/berkeley/path/model_objects/scenario/SensorSet.java
@@ -102,7 +102,50 @@ public class SensorSet extends edu.berkeley.path.model_objects.jaxb.SensorSet  {
 		
 	}
 	
+    /**
+     * Gets the value of the lockedForEdit property.
+     * 
+     * @return boolean
+     *     
+     */
+    @Override
+    public boolean isLockedForEdit() {
+       return super.isLockedForEdit();
+    }
 
+    /**
+     * Sets the value of the lockedForEdit property.
+     * 
+     * @param Boolean value
+     *     
+     */
+    @Override
+    public void setLockedForEdit(Boolean value) {
+    	super.setLockedForEdit(value);
+    }
+
+    /**
+     * Gets the value of the lockedForHistory property.
+     * 
+     * @return boolean
+     *     
+     */
+    @Override
+    public boolean isLockedForHistory() {
+       return super.isLockedForHistory();
+    }
+
+    /**
+     * Sets the value of the lockedForHistory property.
+     * 
+     * @param Boolean value
+     *     
+     */
+    @Override
+    public void setLockedForHistory(Boolean value) {
+    	super.setLockedForHistory(value);
+    }
+    
 	/**
 	 * Set the sensors list. Attaches list of Sensor Model Objects to scenario.
 	 * 
