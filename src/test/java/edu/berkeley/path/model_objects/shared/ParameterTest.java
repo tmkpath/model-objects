899f296b7d5c3af14a34df05963da2f75666394b
Sean Morris <sean.smorris@gmail.com>
2013-05-02 22:26:41 -0700

    added mod stamp

--- src/test/java/edu/berkeley/path/model_objects/shared/ParameterTest.java
+++ src/test/java/edu/berkeley/path/model_objects/shared/ParameterTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 public class ParameterTest {
 	private static String NAME = "Parameter Name";
 	private static String VALUE = "Parameter Value";
+	private static final String MOD_STAMP = "01-APR-1982 23:23:12";
 	private static Parameter p;
 	
 	@Before
@@ -15,13 +16,14 @@ public class ParameterTest {
 		p = new Parameter();
 		p.setName(NAME);
 		p.setValue(VALUE);
+		p.setModStamp(MOD_STAMP);
 	}
 	
 	@Test
 	public void testGetters(){
 		assertEquals(NAME, p.getName());
 		assertEquals(VALUE, p.getValue());
-		
+		assertEquals(MOD_STAMP, p.getModStamp());
 	}
 
 }
