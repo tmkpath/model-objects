88610982f532b3a6f043de192e7922fb30e1b428
mnjuhn <mnjuhn@gmail.com>
2013-10-25 12:54:46 -0700

    Removing package based factories instead using central object factory.

--- src/main/java/edu/berkeley/path/model_objects/ModelObjectsFactory.java
+++ src/main/java/edu/berkeley/path/model_objects/ModelObjectsFactory.java
@@ -27,165 +27,365 @@
 package edu.berkeley.path.model_objects;
 
 import edu.berkeley.path.model_objects.jaxb.ObjectFactory;
-import edu.berkeley.path.model_objects.measurements.PIFProbeData;
-import edu.berkeley.path.model_objects.measurements.PIFProbeDataSet;
-import edu.berkeley.path.model_objects.network.Link;
-import edu.berkeley.path.model_objects.network.Network;
-import edu.berkeley.path.model_objects.network.NetworkFactory;
-import edu.berkeley.path.model_objects.network.Node;
+import edu.berkeley.path.model_objects.measurements.*;
+import edu.berkeley.path.model_objects.network.*;
 import edu.berkeley.path.model_objects.scenario.*;
-import edu.berkeley.path.model_objects.shared.Point;
-import edu.berkeley.path.model_objects.shared.Position;
-import edu.berkeley.path.model_objects.shared.Project;
-import edu.berkeley.path.model_objects.shared.SharedObjectFactory;
+import edu.berkeley.path.model_objects.shared.*;
 
 /**
  * This factory is intended to load a whole scenario from JSON/XML
  * Grouping all existing factories we can get a full model-objects representation instead of some jaxb-based elements
  */
 public class ModelObjectsFactory extends ObjectFactory {
-  private ScenarioFactory scenarioFactory = new ScenarioFactory();
-  private NetworkFactory networkFactory = new NetworkFactory();
-  private SharedObjectFactory sharedFactory = new SharedObjectFactory();
 
+  /**
+   * Create an instance of Actuator Model Object
+   *
+   * @return Actuator Model Object
+   */
   @Override
   public Actuator createActuator() {
     return new Actuator();
   }
 
+  /**
+   * Create an instance of ActuatorSet Model Object
+   *
+   * @return ActuatorSet Model Object
+   */
   @Override
   public ActuatorSet createActuatorSet() {
     return new ActuatorSet();
   }
 
+  /**
+   * Create an instance of Sensor Model Object
+   *
+   * @return Sensor Model Object
+   */
   @Override
   public Sensor createSensor() {
-    return scenarioFactory.createSensor();
+    return new Sensor();
   }
 
+  /**
+   * Create an instance of SensorSet Model Object
+   *
+   * @return SensorSet Model Object
+   */
   @Override
   public SensorSet createSensorSet() {
-    return scenarioFactory.createSensorSet();
+    return new SensorSet();
   }
 
+  /**
+   * Create an instance of Density Model Object
+   *
+   * @return Density Model Object
+   */
   @Override
   public Density createDensity() {
-    return scenarioFactory.createDensity();
+    return new Density();
   }
 
+  /**
+   * Create an instance of InitialDensitySet Model Object
+   *
+   * @return InitialDensitySet Model Object
+   */
   @Override
   public InitialDensitySet createInitialDensitySet() {
-    return scenarioFactory.createInitialDensitySet();
+    return new InitialDensitySet();
   }
 
+  /**
+   * Create an instance of DemandSet Model Object
+   *
+   * @return DemandSet Model Object
+   */
   @Override
   public DemandSet createDemandSet() {
-    return scenarioFactory.createDemandSet();
+    return new DemandSet();
   }
 
+  /**
+   * Create an instance of DemandProfile Model Object
+   *
+   * @return DemandProfile Model Object
+   */
   @Override
   public DemandProfile createDemandProfile() {
-    return scenarioFactory.createDemandProfile();
+    return new DemandProfile();
   }
 
+  /**
+   * Create an instance of FundamentalDiagramSet Model Object
+   *
+   * @return FundamentalDiagramSet Model Object
+   */
   @Override
   public FundamentalDiagramSet createFundamentalDiagramSet() {
-    return scenarioFactory.createFundamentalDiagramSet();
+    return new FundamentalDiagramSet();
   }
 
-  @Override
-  public FundamentalDiagramType createFundamentalDiagramType() {
-    return scenarioFactory.createFundamentalDiagramType();
-  }
-
-  @Override
-  public Splitratio createSplitratio() {
-    return scenarioFactory.createSplitratio();
-  }
-
-  @Override
-  public SplitRatioProfile createSplitRatioProfile() {
-    return scenarioFactory.createSplitRatioProfile();
-  }
-
-  @Override
-  public Demand createDemand() {
-    return scenarioFactory.createDemand();
-  }
-
-  @Override
-  public Scenario createScenario() {
-    return scenarioFactory.createScenario();
-  }
-
-  @Override
-  public SplitRatioSet createSplitRatioSet() {
-    return scenarioFactory.createSplitRatioSet();
-  }
-
-  @Override
-  public PIFProbeData createPIFProbeData() {
-    return scenarioFactory.createPIFProbeData();
-  }
-
-  @Override
-  public PIFProbeDataSet createPIFProbeDataSet() {
-    return scenarioFactory.createPIFProbeDataSet();
-  }
-
-  @Override
-  public Route createRoute() {
-    return scenarioFactory.createRoute();
-  }
-
-  @Override
-  public RouteLink createRouteLink() {
-    return scenarioFactory.createRouteLink();
-  }
-
-  @Override
-  public CalibrationAlgorithmType createCalibrationAlgorithmType() {
-    return scenarioFactory.createCalibrationAlgorithmType();
-  }
-
-  @Override
-  public Network createNetwork() {
-    return networkFactory.createNetwork();
-  }
-
-  @Override
-  public Link createLink() {
-    return networkFactory.createLink();
-  }
-
-  @Override
-  public Node createNode() {
-    return networkFactory.createNode();
-  }
-
-  @Override
-  public Point createPoint() {
-    return sharedFactory.createPoint();
-  }
-
-  @Override
-  public Position createPosition() {
-    return sharedFactory.createPosition();
-  }
-
-  @Override
-  public Project createProject(){
-    return sharedFactory.createProject();
-  }
-
+  /**
+   * Create an instance of FundamentalDiagramProfile Model Object
+   *
+   * @return FundamentalDiagramProfile Model Object
+   */
   @Override
   public FundamentalDiagramProfile createFundamentalDiagramProfile() {
-    return scenarioFactory.createFundamentalDiagramProfile();
+    return new FundamentalDiagramProfile();
   }
 
   @Override
   public FundamentalDiagram createFundamentalDiagram() {
-    return scenarioFactory.createFundamentalDiagram();
+    return new FundamentalDiagram();
+  }
+
+  /**
+   * Create an instance of FundamentalDiagramType Model Object
+   *
+   * @return FundamentalDiagramType Model Object
+   */
+  @Override
+  public FundamentalDiagramType createFundamentalDiagramType() {
+    return new FundamentalDiagramType();
+  }
+
+  /**
+   * Create an instance of Splitratio Model Object
+   *
+   * @return Splitratio Model Object
+   */
+  @Override
+  public Splitratio createSplitratio() {
+    return new Splitratio();
+  }
+
+  /**
+   * Create an instance of SplitRatioProfile Model Object
+   *
+   * @return SplitRatioProfile Model Object
+   */
+  @Override
+  public SplitRatioProfile createSplitRatioProfile() {
+    return new SplitRatioProfile();
+  }
+
+  /**
+   * Create an instance of SplitRatioProfile Model Object
+   *
+   * @return SplitRatioProfile Model Object
+   */
+  @Override
+  public Demand createDemand() {
+    return new Demand();
+  }
+
+  /**
+   * Create an instance of Scenario Model Object
+   *
+   * @return Scenario Model Object
+   */
+  @Override
+  public Scenario createScenario() {
+    return new Scenario();
+  }
+
+  /**
+   * Create an instance of SplitRatioSet  Model Object
+   *
+   * @return SplitRatioSet  Model Object
+   */
+  @Override
+  public SplitRatioSet createSplitRatioSet() {
+
+    return new SplitRatioSet();
+  }
+
+  /**
+   * Create an instance of PIFProbeData  Model Object
+   *
+   * @return PIFProbeData  Model Object
+   */
+  @Override
+  public PIFProbeData createPIFProbeData() {
+    return new PIFProbeData();
+  }
+
+  /**
+   * Create an instance of PIFProbeDataSet  Model Object
+   *
+   * @return PIFProbeDataSet  Model Object
+   */
+  @Override
+  public PIFProbeDataSet createPIFProbeDataSet() {
+    return new PIFProbeDataSet();
+  }
+
+  /**
+   * Create an instance of Route Model Object
+   *
+   * @return Route Model Object
+   */
+  @Override
+  public Route createRoute() {
+    return new Route();
+  }
+
+  /**
+   * Create an instance of RouteLink Model Object
+   *
+   * @return Route Model Object
+   */
+  @Override
+  public RouteLink createRouteLink() {
+    return new RouteLink();
+  }
+
+  /**
+   * Create an instance of createCalibrationAlgorithmType Model Object
+   *
+   * @return createCalibrationAlgorithmType Model Object
+   */
+  @Override
+  public CalibrationAlgorithmType createCalibrationAlgorithmType() {
+    return new CalibrationAlgorithmType();
+  }
+
+  /**
+   * Create an instance of Network Model Object
+   *
+   * @return  Network Model Object
+   */
+  @Override
+  public Network createNetwork() {
+    return new Network();
+  }
+
+  /**
+   * Create an instance of Link Model Object
+   *
+   * @return  Link Model Object
+   */
+  @Override
+  public Link createLink() {
+    return new Link();
+  }
+
+  /**
+   * Create an instance of Node Model Object
+   *
+   * @return  Node Model Object
+   */
+  @Override
+  public Node createNode() {
+    return new Node();
+  }
+
+  /**
+   * Create an instance of Position object
+   * @return Position object
+   */
+  public Position createPosition(){
+    return new Position();
+  }
+
+  /**
+   * Create an instance of Point object
+   * @return Position object
+   */
+  public Point createPoint(){
+    return new Point();
+  }
+
+  /**
+   * Create an instance of Project object
+   * @return Project object
+   */
+  public Project createProject(){
+    return new Project();
+  }
+  /**
+   * Create an instance of PeMSDatum object
+   * @return PeMSDatum object
+   */
+  @Override
+  public PeMSDatum createPeMSDatum(){
+    return new PeMSDatum();
+  }
+
+  /**
+   * Create an instance of PeMSStationAggregate object
+   * @return PeMSStationAggregate object
+   */
+  @Override
+  public PeMSStationAggregate createPeMSStationAggregate(){
+    return new PeMSStationAggregate();
+  }
+
+  /**
+   * Create an instance of PeMSDataSet object
+   * @return PeMSDataSet object
+   */
+  @Override
+  public PeMSDataSet createPeMSDataSet(){
+    return new PeMSDataSet();
+  }
+
+  /**
+   * Create an instance of PeMSDataProfile object
+   * @return PeMSDataProfile object
+   */
+  @Override
+  public PeMSDataProfile createPeMSDataProfile(){
+    return new PeMSDataProfile();
+  }
+
+  /**
+   * Create an instance of PeMSAggregate object
+   * @return PeMSAggregate object
+   */
+  @Override
+  public PeMSAggregate createPeMSAggregate(){
+    return new PeMSAggregate();
+  }
+
+  /**
+   * Create an instance of VDS object
+   * @return PeMSDatum object
+   */
+  @Override
+  public VDS createVDS(){
+    return new VDS();
+  }
+
+  /**
+   * Create an instance of PeMSAggregateDatum object
+   * @return PeMSAggregateDatum object
+   */
+  @Override
+  public PeMSAggregateDatum createPeMSAggregateDatum(){
+    return new PeMSAggregateDatum();
+  }
+
+  /**
+   * Create an instance of PeMSAggregateDataProfile object
+   * @return PeMSAggregateDataProfile object
+   */
+  @Override
+  public PeMSAggregateDataProfile createPeMSAggregateDataProfile(){
+    return new PeMSAggregateDataProfile();
+  }
+
+  /**
+   * Create an instance of PeMSAggregateDataSet object
+   * @return PeMSAggregateDataSet object
+   */
+  @Override
+  public PeMSAggregateDataSet createPeMSAggregateDataSet(){
+    return new PeMSAggregateDataSet();
   }
 
 }
