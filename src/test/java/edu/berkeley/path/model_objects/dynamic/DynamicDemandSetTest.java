8a5d99fd0d425cb5dab70dd2682e01d6075bd625
javierhe <javierhe@gmail.com>
2013-10-28 10:09:15 -0700

    Test new fields on DemandSet

--- src/test/java/edu/berkeley/path/model_objects/dynamic/DynamicDemandSetTest.java
+++ src/test/java/edu/berkeley/path/model_objects/dynamic/DynamicDemandSetTest.java
@@ -3,6 +3,7 @@ package edu.berkeley.path.model_objects.dynamic;
 import edu.berkeley.path.model_objects.MOException;
 import edu.berkeley.path.model_objects.TestConfiguration;
 import edu.berkeley.path.model_objects.shared.CrudFlag;
+import edu.berkeley.path.model_objects.shared.DateTime;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,11 +17,14 @@ import static org.junit.Assert.assertNull;
 public class DynamicDemandSetTest {
 
   private static final long PROJECT_ID = 10;
+  private static final int APP_RUN_ID = 20;
   private static final String DESCRIPTION = "Test Set";
   private static final String NAME = "Test Set Name";
 
   private static final long ID = 1;
+  private static final long MS = System.currentTimeMillis();
   private static final String MOD_STAMP = "01-APR-1982 23:23:12";
+
 
   private DynamicDemandSet set;
 
@@ -34,6 +38,10 @@ public class DynamicDemandSetTest {
     set.setId(ID);
     set.setName(NAME);
     set.setModStamp(MOD_STAMP);
+    set.setAppRunId(APP_RUN_ID);
+    DateTime dt = new DateTime();
+    dt.setMilliseconds(MS);
+    set.setEstTstamp(dt);
     set.setLockedForEdit(true);
     set.setLockedForHistory(true);
 
@@ -54,6 +62,9 @@ public class DynamicDemandSetTest {
     assertEquals(ID,set.getId());
     assertEquals(NAME, set.getName());
     assertEquals(MOD_STAMP, set.getModStamp());
+    assertEquals(APP_RUN_ID, set.getAppRunId());
+
+    assertEquals(MS, set.getEstTstamp().getMilliseconds());
     assertEquals(true, set.isLockedForEdit());
     assertEquals(true, set.isLockedForHistory());
     assertEquals(3, set.getListOfDemandProfiles().size());
